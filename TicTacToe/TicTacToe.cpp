04c3ac9e7cbb19e4db14fcc25a756c20a30b5326
s198061 <s198061@LAF-7E868C>
2020-01-29 12:59:59 -0600

    make the player choice what spot

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -24,16 +24,115 @@ void board()
 	cout << "[" << square[4] << "]" << "[" << square[5] << "]" << "[" << square[6] << "]" << endl;
 	cout << "[" << square[7] << "]" << "[" << square[8] << "]" << "[" << square[9] << "]" << endl;
 	cout << endl;
+
+
 }
 
 int checkWin()
 {
-	return 1;
+	//if one of the players match the first row
+ 	if (square[1] == square[2] && square[2] == square[3])
+		return 1;
+
+	//if one of the players match the second row
+	else if (square[4] == square[5] && square[5] == square[6])
+		return 1;
+
+	//if one of the players match the third row
+	else if (square[7] == square[8] && square[8] == square[9])
+		return 1;
+
+	//if one of the players match the first column
+	else if (square[1] == square[4] && square[4] == square[7])
+		return 1;
+
+	//if one of the players match the second column
+	else if (square[2] == square[5] && square[5] == square[8])
+		return 1;
+
+	//if one of the players match the third column
+	else if (square[3] == square[6] && square[6] == square[9])
+		return 1;
+
+	//if one of the players match diagonally left to right
+	else if (square[1] == square[5] && square[5] == square[9])
+		return 1;
+
+	//if one of the players match diagonally right to left
+	else if (square[3] == square[5] && square[5] == square[7])
+		return 1;
+
+	//if the players CAT the game
+	else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
+		&& square[4] != '4' && square[5] != '5' && square[6] != '6'
+		&& square[7] != '7' && square[8] != '8' && square[9] != '9')
+		return -1;
+
+	//if game is over
+	else
+		return 0;
 }
 
 int main()
 {
-	board();
+	char mark;
+	int player = 1,i,choice;
+
+	while (checkWin() == 2)
+	{
+		//draws the borad and welcome message
+		board();
+		player = (player % 2) ? 1 : 2;
+
+		cout << "Player " << player << ", enter a number: ";
+		cin >> choice;
+
+		mark = (player == 1) ? 'X' : 'O';
+
+		if (choice == 1 && square[1] == '1')
+			square[1] = mark;
+
+		else if (choice == 2 && square[2] == '2')
+			square[2] = mark;
+
+		else if (choice == 3 && square[3] == '3')
+			square[3] = mark;
+
+		else if (choice == 4 && square[4] == '4')
+			square[4] = mark;
+
+		else if (choice == 5 && square[5] == '5')
+			square[5] = mark;
+
+		else if (choice == 6 && square[6] == '6')
+			square[6] = mark;
+
+		else if (choice == 7 && square[7] == '7')
+			square[7] = mark;
+
+		else if (choice == 8 && square[8] == '8')
+			square[8] = mark;
+
+		else if (choice == 9 && square[9] == '9')
+			square[9] = mark;
+		else
+		{
+			cout << "Invaild Move";
+			player--;
+			cin.ignore();
+			cin.get();
+		}
+
+		i = checkWin();
+
+		player++;
+
+		if (i == 1)
+			cout << "Player " << --player << " wins!";
+
+	}
+	
+
 
 }
 
